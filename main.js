4f8519ef35f968c482b75fbd4ae1ce30d66f42fa
Anu820 <87087735+Anu820@users.noreply.github.com>
2021-08-17 18:06:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("My first consoal project");
+
+var first_name = "Ranvir";
+var last_name = "Kapoor";
+
+var ful_name = first_name.concat(last_name);
+  console.log(full_name);
+
+  var a = 10;
+  var b = 5;
+
+  var add = a + b ;
+  console.log (add);
+
+  var sub = a - b ;
+  console.log (sub);
+
+  var multi = a * b ;
+  console.log (multi);
+
+  var div = a / b ;
+  console.log (div);
